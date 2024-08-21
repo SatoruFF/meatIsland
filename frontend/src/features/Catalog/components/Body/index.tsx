d02d0c22193ab7dc4272d505e2c2b9b09fd797cb
Airat Kalimov <airat.kalimov@gmail.com>
2024-08-22 00:35:59 +0300

    merge

--- frontend/src/features/Catalog/components/Body/index.tsx
+++ frontend/src/features/Catalog/components/Body/index.tsx
@@ -1,17 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 
-import CategoryContainer from "./CategoryContainer";
+import CategoryContainer from "./components/CategoryContainer";
 
 import routes from "../../../../shared/utils/routes";
 
 import styles from "./stylesBody.module.less";
+import Basket from "./components/Basket";
 
 const CatalogBody = () => {
   return (
-    <div className={styles.catalogBody}>
-      <Routes>
-        <Route path={routes.category} element={<CategoryContainer />} />
-      </Routes>
+    <div className={styles.catalog}>
+      <Basket />
+      <div className={styles.categoryContainer}>
+        <Routes>
+          <Route path={routes.category} element={<CategoryContainer />} />
+        </Routes>
+      </div>
     </div>
   );
 };
--- frontend/src/features/Catalog/index.tsx
+++ frontend/src/features/Catalog/index.tsx
@@ -1,4 +1,6 @@
+import Basket from "./components/Body/Basket";
 import CatalogBody from "./components/Body/index";
+import CategoryList from "./components/Body/index";
 import Footer from "./components/Footer/index";
 import Header from "./components/Header/index";
 

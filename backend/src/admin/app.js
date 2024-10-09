f7dee5d66bcfedf89b589a8429608f52daee4186
Holden Caulfield <sasha.mikheevv@gmail.com>
2024-10-09 23:48:33 +0300

    feat: add locales to strapi

---
+++ backend/src/admin/app.js
@@ -0,0 +1,16 @@
+const config = {
+    locales: [
+        'en',
+        'ru'
+    ],
+  };
+  
+  const bootstrap = (app) => {
+    console.log(app);
+  };
+  
+  export default {
+    config,
+    bootstrap,
+  };
+  

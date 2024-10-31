9a618b342cabf077f1460a35784e59601603c13d
IlyasKham999 <89062748+IlyasKham999@users.noreply.github.com>
2024-10-31 18:20:11 +0300

    fix(Configs): change config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // url: 'https://meatisland.ru/core',
+  url: "https://meatisland.ru/core",
   app: {
     keys: env.array("APP_KEYS"),
   },

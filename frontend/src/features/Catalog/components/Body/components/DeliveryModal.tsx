257a5c62fee239842728dd0eccbd34f717b5b89d
IlyasKham999 <89062748+IlyasKham999@users.noreply.github.com>
2024-11-11 16:48:37 +0300

    feat(DeliveryModal): modal at successful checkout

--- frontend/src/features/Catalog/components/Body/components/DeliveryModal.tsx
+++ frontend/src/features/Catalog/components/Body/components/DeliveryModal.tsx
@@ -11,8 +11,15 @@ const { TextArea } = Input;
 
 const DeliveryModal = ({ items, isOpen, onClose, countProduct, sumPrice }) => {
   const [modalWidth, setModalWidth] = useState("50%");
+  const [successfulOrder, setSuccessfulOrder] = useState(false);
+
   const [form] = Form.useForm();
   const { basket } = basketStore();
+
+  const onCloseModal = () => {
+    setSuccessfulOrder(false);
+    onClose();
+  };
 
   const handleSubmit = async (values: ISaleAttrs) => {
     try {
@@ -27,6 +34,7 @@ const DeliveryModal = ({ items, isOpen, onClose, countProduct, sumPrice }) => {
               }),
         }),
       }); // TODO: удалить это стремный костыль
+      setSuccessfulOrder(true);
     } catch (error) {
       message.error(
         `Что то пошло не так при оформлении заказа, текст ошибки: ${error.message}}`
@@ -62,77 +70,86 @@ const DeliveryModal = ({ items, isOpen, onClose, countProduct, sumPrice }) => {
       title={<h2 className={styles.deliveryModalTitle}>Доставка</h2>}
       open={isOpen}
       footer={null}
-      onCancel={onClose}
+      onCancel={onCloseModal}
       width={modalWidth}
       wrapClassName={styles.deliveryModal}
     >
-      <Form
-        form={form}
-        onFinish={handleSubmit}
-        className={styles.deliveryForm}
-        initialValues={{ deliveryMethod: "delivery" }}
-      >
-        <Form.Item
-          className={styles.formInputContainer}
-          name="name"
-          rules={[{ required: true, message: "Пожалуйста, введите ваше имя!" }]}
+      {successfulOrder ? (
+        <div className={styles.successfulOrderContainer}>
+          <div>Заказ успешно оформлен 🎉</div>
+          <div>Ближайшее время вам позвонит наш менеджер</div>
+        </div>
+      ) : (
+        <Form
+          form={form}
+          onFinish={handleSubmit}
+          className={styles.deliveryForm}
+          initialValues={{ deliveryMethod: "delivery" }}
         >
-          <Input className={styles.formInput} placeholder="Ваше имя" />
-        </Form.Item>
+          <Form.Item
+            className={styles.formInputContainer}
+            name="name"
+            rules={[
+              { required: true, message: "Пожалуйста, введите ваше имя!" },
+            ]}
+          >
+            <Input className={styles.formInput} placeholder="Ваше имя" />
+          </Form.Item>
 
-        <Form.Item
-          name="phone"
-          rules={[
-            { required: true, message: "Пожалуйста, введите ваш телефон!" },
-          ]}
-        >
-          <Input className={styles.formInput} placeholder="Телефон" />
-        </Form.Item>
+          <Form.Item
+            name="phone"
+            rules={[
+              { required: true, message: "Пожалуйста, введите ваш телефон!" },
+            ]}
+          >
+            <Input className={styles.formInput} placeholder="Телефон" />
+          </Form.Item>
 
-        <Form.Item name="deliveryMethod">
-          <Radio.Group className={styles.radioBtn}>
-            <Radio className={styles.radioBtn} value="selfPickup">
-              Самовывоз
-            </Radio>
-            <Radio className={styles.radioContainer} value="delivery">
-              Доставка
-            </Radio>
-          </Radio.Group>
-        </Form.Item>
+          <Form.Item name="deliveryMethod">
+            <Radio.Group className={styles.radioBtn}>
+              <Radio className={styles.radioBtn} value="selfPickup">
+                Самовывоз
+              </Radio>
+              <Radio className={styles.radioContainer} value="delivery">
+                Доставка
+              </Radio>
+            </Radio.Group>
+          </Form.Item>
 
-        <Form.Item
-          name="address"
-          rules={[{ required: true, message: "Пожалуйста, введите адрес!" }]}
-        >
-          <Input
-            className={styles.formInput}
-            placeholder="Адрес (улица, дом, квартира)"
-          />
-        </Form.Item>
-        <Form.Item name="floor">
-          <Input className={styles.formInput} placeholder="Этаж" />
-        </Form.Item>
-        <Form.Item name="intercom">
-          <Input className={styles.formInput} placeholder="Домофон" />
-        </Form.Item>
+          <Form.Item
+            name="address"
+            rules={[{ required: true, message: "Пожалуйста, введите адрес!" }]}
+          >
+            <Input
+              className={styles.formInput}
+              placeholder="Адрес (улица, дом, квартира)"
+            />
+          </Form.Item>
+          <Form.Item name="floor">
+            <Input className={styles.formInput} placeholder="Этаж" />
+          </Form.Item>
+          <Form.Item name="intercom">
+            <Input className={styles.formInput} placeholder="Домофон" />
+          </Form.Item>
 
-        <Form.Item name="comment">
-          <TextArea
-            rows={4}
-            className={styles.formInput}
-            placeholder="Комментарий к заказу"
-          />
-        </Form.Item>
+          <Form.Item name="comment">
+            <TextArea
+              rows={4}
+              className={styles.formInput}
+              placeholder="Комментарий к заказу"
+            />
+          </Form.Item>
 
-        <Form.Item>
-          <div className={styles.titleBtnTop}>
-            Доставка осушествляеться только по городу Казань с 9:00 - 20:00
-          </div>
-          <Button type="primary" htmlType="submit" className={styles.formBtn}>
-            Оформить
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <div className={styles.titleBtnTop}>
+              Доставка осушествляеться только по городу Казань с 9:00 - 20:00
+            </div>
+            <Button type="primary" htmlType="submit" className={styles.formBtn}>
+              Оформить
+            </Button>
+          </Form.Item>
+        </Form>
+      )}
     </Modal>
   );
 };
